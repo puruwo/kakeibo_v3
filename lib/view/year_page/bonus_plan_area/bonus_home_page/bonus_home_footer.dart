870d5898bb17e21b9906f1af9ace975b22a7c973
puruwo <jo3kwbto@icloud.com>
2025-06-13 06:53:09 +0900

    ✈️feat_ボーナスプランフッターボタンの機能実装

--- lib/view/year_page/bonus_plan_area/bonus_home_page/bonus_home_footer.dart
+++ lib/view/year_page/bonus_plan_area/bonus_home_page/bonus_home_footer.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:kakeibo/constant/colors.dart';
 import 'package:kakeibo/constant/strings.dart';
+import 'package:kakeibo/domain/db/expense/expense_entity.dart';
+import 'package:kakeibo/domain/db/income/income_entity.dart';
+import 'package:kakeibo/view/register_page/expense_tab/register_expense_page.dart';
+import 'package:kakeibo/view/register_page/income_tab/register_income_page.dart';
 import 'package:kakeibo/view_model/state/bonus_home_page/selected_tab_controller/selected_tab_controller.dart';
 
 class BonusHomeFooter extends ConsumerWidget {
@@ -11,19 +16,51 @@ class BonusHomeFooter extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     switch (ref.watch(selectedTabControllerNotifierProvider)) {
       case SelectedTab.bonusExpense:
-        return _newExpenseButton();
+        return _newExpenseButton(context, ref);
       case SelectedTab.bonusIncome:
-        return _newIncomeButton();
+        return _newIncomeButton(context, ref);
       default:
         return const SizedBox.shrink(); // 他のタブでは何も表示しない
     }
   }
 
-  Widget _newExpenseButton() {
+  Widget _newExpenseButton(BuildContext context, WidgetRef ref) {
     return SizedBox(
       width: double.infinity,
       child: ElevatedButton(
         onPressed: () {
+          final today = DateTime.now();
+          ExpenseEntity newExpense = ExpenseEntity(
+            date: DateFormat('yyyyMMdd').format(today),
+            incomeSourceBigCategory: 1, //　1を指定することで、収入の大カテゴリーでボーナスを選択する
+            memo: '',
+          );
+
+          showModalBottomSheet(
+            //sccafoldの上に出すか
+            useRootNavigator: true,
+            isScrollControlled: true,
+            useSafeArea: true,
+            constraints: const BoxConstraints(
+              maxWidth: 2000,
+            ),
+            context: context,
+            // constで呼び出さないとリビルドがかかってtextfieldのも何度も作り直してしまう
+            builder: (context) {
+              return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                theme: ThemeData.dark(),
+                themeMode: ThemeMode.dark,
+                darkTheme: ThemeData.dark(),
+                home: MediaQuery.withClampedTextScaling(
+                  child: RegisterExpensePage(
+                    expenseEntity: newExpense,
+                    isTabVisible: true, // タブを非表示にする
+                  ),
+                ),
+              );
+            },
+          );
         },
         style: ElevatedButton.styleFrom(
           backgroundColor: MyColors.buttonPrimary,
@@ -36,11 +73,43 @@ class BonusHomeFooter extends ConsumerWidget {
     );
   }
 
-  Widget _newIncomeButton() {
+  Widget _newIncomeButton(BuildContext context, WidgetRef ref) {
     return SizedBox(
       width: double.infinity,
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            //sccafoldの上に出すか
+            useRootNavigator: true,
+            isScrollControlled: true,
+            useSafeArea: true,
+            constraints: const BoxConstraints(
+              maxWidth: 2000,
+            ),
+            context: context,
+            // constで呼び出さないとリビルドがかかってtextfieldのも何度も作り直してしまう
+            builder: (context) {
+              final today = DateTime.now();
+              IncomeEntity newIncome = IncomeEntity(
+                date: DateFormat('yyyyMMdd').format(today),
+                categoryId: 1, //　1を指定することで、でボーナスを選択する
+                memo: 'oeiaoi',
+              );
+              return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                theme: ThemeData.dark(),
+                themeMode: ThemeMode.dark,
+                darkTheme: ThemeData.dark(),
+                home: MediaQuery.withClampedTextScaling(
+                  child: RegisterIncomePage(
+                    incomeEntity: newIncome,
+                    isTabVisible: true, // タブを非表示にする
+                  ),
+                ),
+              );
+            },
+          );
+        },
         style: ElevatedButton.styleFrom(
           backgroundColor: MyColors.buttonPrimary,
         ),
