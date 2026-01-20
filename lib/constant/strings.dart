7862f19cc202eaaba577537feb268cad119a4dcc
puruwo <jo3kwbto@icloud.com>
2026-01-20 19:05:01 +0900

    🐞bugfix_スタイル修正のコミット漏れ

--- lib/constant/strings.dart
+++ lib/constant/strings.dart
@@ -2,7 +2,6 @@ export 'package:kakeibo/constant/colors.dart';
 export 'package:kakeibo/constant/styles/app_text_styles.dart';
 export 'package:kakeibo/constant/styles/monthly_page_styles.dart';
 export 'package:kakeibo/constant/styles/calendar_styles.dart';
-export 'package:kakeibo/constant/styles/history_list_styles.dart';
 export 'package:kakeibo/constant/styles/register_page_styles.dart';
 export 'package:kakeibo/constant/styles/yearly_income_list_styles.dart';
 export 'package:kakeibo/constant/styles/budget_settings_styles.dart';
