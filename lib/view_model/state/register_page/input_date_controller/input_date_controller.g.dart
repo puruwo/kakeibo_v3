5bdab5b485e218a828d2bf2996b3b3815142f6c1
puruwo <jo3kwbto@icloud.com>
2026-03-31 14:13:21 +0900

    ⚒️infra_flutterバージョンアップ対応

--- lib/view_model/state/register_page/input_date_controller/input_date_controller.g.dart
+++ lib/view_model/state/register_page/input_date_controller/input_date_controller.g.dart
@@ -22,6 +22,6 @@ final inputDateControllerNotifierProvider =
   allTransitiveDependencies: null,
 );
 
-typedef _$inputDateControllerNotifier = AutoDisposeNotifier<DateTime>;
+typedef _$InputDateControllerNotifier = AutoDisposeNotifier<DateTime>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
