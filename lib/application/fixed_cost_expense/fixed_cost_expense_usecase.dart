29a45e5df8c23af2939a44d403d3f2965ed6a857
puruwo <jo3kwbto@icloud.com>
2026-08-11 11:07:44 +0900

    🐞bugfix_👾UT 固定費確定時の想定額更新をawaitし完了前に画面リフレッシュが走る競合を解消

--- lib/application/fixed_cost_expense/fixed_cost_expense_usecase.dart
+++ lib/application/fixed_cost_expense/fixed_cost_expense_usecase.dart
@@ -43,8 +43,10 @@ class FixedCostExpenseUsecase {
       price: confirmedPrice,
     );
 
-    _fixedCostUsecaseRepositoryProvider.updateEstimatedPrice(
-        fixedCostId: tileValue.fixedCostId);
+    // 想定額の更新が終わってからDBの更新回数を進めるため、完了まで待つ
+    await _fixedCostUsecaseRepositoryProvider.updateEstimatedPrice(
+      fixedCostId: tileValue.fixedCostId,
+    );
 
     // DBの更新回数をインクリメント
     updateDBCountNotifier.incrementState();
@@ -59,9 +61,7 @@ class FixedCostExpenseUsecase {
   }
 
   /// 固定費支出を編集する
-  Future<void> edit({
-    required FixedCostExpenseEntity entity,
-  }) async {
+  Future<void> edit({required FixedCostExpenseEntity entity}) async {
     // エラーチェック
     if (entity.price <= 0) {
       throw const AppException('0円以上で入力してください');
